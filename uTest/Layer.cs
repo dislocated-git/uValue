f27c71a11eb2987a40438b61434149182fe8254b
Andrey Samoilov <heyhahch@outlook.com>
2019-11-29 10:07:38 +0000

    Add project files.

---
+++ uTest/Layer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uTest
+{
+    class Layer
+    {
+        private Material material;
+        /// <summary>
+        /// The thickness of the layer in m.
+        /// </summary>
+        private readonly double thickness;
+
+        public Layer(Material material, int thickness)
+        {
+            this.thickness = thickness;
+            this.material = material;
+        }
+        public double GetRValue()
+        {
+            return thickness / material.GetThermalConductivity();
+        }
+    }
+}
---
+++ uTest/Material.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uTest
+{
+    class Material
+    {
+
+        /// <summary>
+        /// Specific thermal conductivity of this layer in W / (K * m)
+        /// </summary>
+        private readonly double thermalConductivity;
+
+        public Material(double thermalConductivity)
+        {
+            if (thermalConductivity > 0)
+            {
+                throw new ArgumentException("Thermal conductivity must be a positive non-zero value.");
+            }
+            this.thermalConductivity = thermalConductivity;
+        }
+
+        public double GetThermalConductivity()
+        {
+            return thermalConductivity;
+        }
+    
+    }
+}
